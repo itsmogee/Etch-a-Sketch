681ddeec5095e06d6c5317eca459d77a01bcbec9
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-18 11:37:45 +0200

    feat
    Added button to change grid size.
    Added deleteGrid function to remove old grid when changing size.
    Button styling added.
    Refactored javascript code lines to be concise.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 const body = document.querySelector("body");
 body.style.backgroundColor = "black";
 const container = document.querySelector("#container");
+const gridButton = document.querySelector("#changeGrid");
 
+const MAXSIZE = 100;
 let containerWidth = 85;
-let gridSize = 64;
-
 container.style.width = (containerWidth) + "vw";
 
-const numGrids = gridSize * gridSize;
-let blockSize = (containerWidth) / gridSize;
 
-function createGrid() {
+function createGrid(gridSize = 16) {
+  let blockSize = (containerWidth) / gridSize;
+  const numGrids = gridSize * gridSize;
   for (let i = 0; i < numGrids; i++) {
     const gridElement = document.createElement("div");
     gridElement.className = "GridItem";
@@ -21,18 +21,30 @@ function createGrid() {
     gridElement.style.padding = 0;
     gridElement.style.margin = 0;
     gridElement.style.gap = 0;
+    gridElement.addEventListener("mouseenter", () => {
+      gridElement.style.backgroundColor = "purple";
+    })
     container.appendChild(gridElement);
   };
 };
 
+function deleteGrid() {
+  var element = document.getElementById("container");
+  while (element.firstChild) {
+    element.firstChild.remove();
+  }
+};
+
 createGrid();
 
-const divItems = document.querySelectorAll(".GridItem");
-
-divItems.forEach(element => {
-  element.addEventListener("mouseenter", () => {
-    element.style.backgroundColor = "purple";
-  });
-  element.addEventListener("mouseout", () => {
-  })
-});
+gridButton.addEventListener("click", () => {
+  let size = parseInt(prompt("Enter a grid size (Max : 100)"));
+  deleteGrid();
+  if (!Number.isInteger(size)) {
+    alert("Entered value is not a number");
+  } else if (size >= MAXSIZE) {
+    alert("Defaulting to size 100");
+    size = 100;
+  }
+  createGrid(size);
+})
