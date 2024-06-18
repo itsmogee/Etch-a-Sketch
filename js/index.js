b240268fe5391066e8c5d8af7440f3c7d8ad549a
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-18 09:09:03 +0200

    feat
    Adds event listener for mouse hovering over grid and changes color.
    Background changed to black.
    Styling elements added.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 const body = document.querySelector("body");
+body.style.backgroundColor = "black";
 const container = document.querySelector("#container")
 
 function createGrid() {
@@ -7,8 +8,23 @@ function createGrid() {
     gridElement.style.width = "100px";
     gridElement.style.height = "100px";
     gridElement.textContent = "Grid : " + parseInt(i + 1);
+    gridElement.style.backgroundColor = "orange";
+    gridElement.style.borderColor = "white";
+    gridElement.style.borderWidth = "thin";
+    gridElement.style.borderStyle = "solid";
     container.appendChild(gridElement);
   };
 };
 
 createGrid();
+
+const divItems = document.querySelectorAll(".GridItem");
+
+divItems.forEach(element => {
+  element.addEventListener("mouseenter", () => {
+    element.style.backgroundColor = "purple";
+  });
+  element.addEventListener("mouseout", () => {
+    element.style.backgroundColor = "orange";
+  })
+});
