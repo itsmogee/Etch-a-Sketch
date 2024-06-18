d1f7e7f5914346b24c80e3db90ec9b5bca5abb9e
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-06-18 14:36:53 +0200

    feat Added a color randomizer. Background color changed to lighter shade of green. Added a darkening effect when revisiting grid nodes.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,31 @@
 const body = document.querySelector("body");
-body.style.backgroundColor = "black";
+body.style.backgroundColor = "rgb(0, 60, 50)";
 const container = document.querySelector("#container");
 const gridButton = document.querySelector("#changeGrid");
 
 const MAXSIZE = 100;
-let containerWidth = 52;
+let containerWidth = 40;
 container.style.width = (containerWidth) + "vw";
 
 
 function createGrid(gridSize = 16) {
   let blockSize = (containerWidth) / gridSize;
+  let opacityValues = [];
   const numGrids = gridSize * gridSize;
   for (let i = 0; i < numGrids; i++) {
     const gridElement = document.createElement("div");
     gridElement.className = "GridItem";
+    gridElement.title = i;
     gridElement.style.display = "flex";
     gridElement.style.width = blockSize + "vw";
     gridElement.style.height = blockSize * (16 / 9) + "vh";
-    gridElement.style.backgroundColor = "orange";
+    gridElement.style.backgroundColor = "white";
     gridElement.style.padding = 0;
     gridElement.style.margin = 0;
     gridElement.style.gap = 0;
+    opacityValues.push(1);
     gridElement.addEventListener("mouseenter", () => {
-      gridElement.style.backgroundColor = "purple";
+      gridElement.style.backgroundColor = rgbRandomizer(opacityValues, gridElement);
     })
     container.appendChild(gridElement);
   };
@@ -50,3 +53,15 @@ gridButton.addEventListener("click", () => {
   }
   createGrid(size);
 })
+
+function rgbRandomizer(opacityValues, gridElement) {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+
+  if (opacityValues[gridElement.title] > 0) {
+    opacityValues[gridElement.title] -= 0.1;
+  }
+
+  return "rgba(" + r + "," + g + "," + b + "," + opacityValues[gridElement.title] + ")";
+}
